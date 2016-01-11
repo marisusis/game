5aad92f80f68ec8f07bd121f717ffdcecc8c174f
spacegeek224 <marisusisme@gmail.com>
2016-01-11 17:01:16 +0000

    mysql

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,3 +2,22 @@ var mysql = require('mysql');
 var colors = require('colors');
 var log = require('util').log;
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "6277"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
